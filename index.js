24a83edebd8df9a3f92469e566018c1690288234
fireteam99 <raymancheungsy@gmail.com>
2022-01-14 17:49:28 -0500

    updated font sizing and made phone number optional

--- index.js
+++ index.js
@@ -1,38 +1,67 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const Handlebars = require("handlebars");
 const { DateTime } = require("luxon");
 
-Handlebars.registerHelper('formatDate', function(dateString) {
-    let specificity = dateString.split('-').length;
+Handlebars.registerHelper("formatDate", function (dateString) {
+  let specificity = dateString.split("-").length;
 
-    dt = DateTime.fromISO(dateString);
-    return  dt.toLocaleString(specificity === 3 ? DateTime.DATE_MED : { month: 'short', year: 'numeric' })
-})
+  dt = DateTime.fromISO(dateString);
+  return dt.toLocaleString(
+    specificity === 3 ? DateTime.DATE_MED : { month: "short", year: "numeric" }
+  );
+});
+
+Handlebars.registerHelper("ifCond", function (v1, operator, v2, options) {
+  switch (operator) {
+    case "==":
+      return v1 == v2 ? options.fn(this) : options.inverse(this);
+    case "===":
+      return v1 === v2 ? options.fn(this) : options.inverse(this);
+    case "!=":
+      return v1 != v2 ? options.fn(this) : options.inverse(this);
+    case "!==":
+      return v1 !== v2 ? options.fn(this) : options.inverse(this);
+    case "<":
+      return v1 < v2 ? options.fn(this) : options.inverse(this);
+    case "<=":
+      return v1 <= v2 ? options.fn(this) : options.inverse(this);
+    case ">":
+      return v1 > v2 ? options.fn(this) : options.inverse(this);
+    case ">=":
+      return v1 >= v2 ? options.fn(this) : options.inverse(this);
+    case "&&":
+      return v1 && v2 ? options.fn(this) : options.inverse(this);
+    case "||":
+      return v1 || v2 ? options.fn(this) : options.inverse(this);
+    default:
+      return options.inverse(this);
+  }
+});
 
 function render(resume) {
-    var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-    var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
-    var partialsDir = path.join(__dirname, 'partials');
-    var filenames = fs.readdirSync(partialsDir);
+  var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+  var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+  var partialsDir = path.join(__dirname, "partials");
+  var filenames = fs.readdirSync(partialsDir);
 
-    filenames.forEach(function(filename) {
-        var matches = /^([^.]+).hbs$/.exec(filename);
-        if (!matches) {
-            return;
-        }
-        var name = matches[1];
-        var filepath = path.join(partialsDir, filename)
-        var template = fs.readFileSync(filepath, 'utf8');
+  filenames.forEach(function (filename) {
+    var matches = /^([^.]+).hbs$/.exec(filename);
+    if (!matches) {
+      return;
+    }
+    var name = matches[1];
+    var filepath = path.join(partialsDir, filename);
+    var template = fs.readFileSync(filepath, "utf8");
 
-        Handlebars.registerPartial(name, template);
-    });
-    return Handlebars.compile(tpl)({
-        css: css,
-        resume: resume
-    });
+    Handlebars.registerPartial(name, template);
+  });
+  return Handlebars.compile(tpl)({
+    css: css,
+    resume: resume,
+  });
 }
 
 module.exports = {
-    render: render
+  render: render,
 };
